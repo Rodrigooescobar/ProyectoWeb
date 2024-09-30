e19cd40f9e09d3654ddba2ea3fa649f58a63054e
Rodrigo Escobar <rodrigo_99_88@hotmail.com>
2024-09-30 15:17:25 -0300

    login

---
+++ Proyecto/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        } 
+        
+        public IActionResult CerrarSesion()
+        {
+            //action - controler
+            return RedirectToAction("Login", "login");
+        }
+    }
+}
